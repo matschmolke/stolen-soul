a3edea2f022846eebe34d2d71d3c6e9d521b4ea5
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-11-02 20:55:42 +0100

    Skrypt dodania itemów do ekwipunku; Slot prefab

--- Assets/_GAME_/Scripts/InventoryManager.cs
+++ Assets/_GAME_/Scripts/InventoryManager.cs
@@ -5,6 +5,7 @@ public class InventoryManager : MonoBehaviour
     public GameObject InventoryMenu;
 
     private bool menuActivated;
+    public ItemSlot[] itemSlot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,6 +26,18 @@ public class InventoryManager : MonoBehaviour
             Time.timeScale = 0;
             InventoryMenu.SetActive(true);
             menuActivated = true;
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if(itemSlot[i].isFull == false)
+            {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite);
+                return;
+            }
         }
     }
 }
