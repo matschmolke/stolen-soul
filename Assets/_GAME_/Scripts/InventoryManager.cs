7453074aabcba4c7d47b6f4c4025f1517c72bf53
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-11-02 23:46:20 +0100

    Item script. Obtaining items on the floor logic.

--- Assets/_GAME_/Scripts/InventoryManager.cs
+++ Assets/_GAME_/Scripts/InventoryManager.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public static InventoryManager Instance { get; private set; }
     public GameObject InventoryMenu;
-
+    
     private bool menuActivated;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,5 +39,10 @@ public class InventoryManager : MonoBehaviour
             InventoryMenu.SetActive(true);
             menuActivated = true;
         }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName = " + itemName +  ", quantity = " + quantity);
     }
 }
