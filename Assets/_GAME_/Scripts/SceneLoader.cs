e92c4157fed74c523a0308e78f5ee3f22e51b36a
emilia186 <emiliakorn59@gmail.com>
2025-11-02 13:51:30 +0100

    Added player, collisions and camera controller

---
+++ Assets/_GAME_/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        //loads all scenes together
+        SceneManager.LoadSceneAsync("TutorialDung", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync("InventoryScene", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync("PlayerScene", LoadSceneMode.Additive);
+    }
+}
