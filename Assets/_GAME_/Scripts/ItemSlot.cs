60210c904f95359155bd6c654cadb5d3d0a7eb3d
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-11-03 02:40:54 +0100

    Drag & Drop (przemieszczenie itemów w ekwipunku).

--- Assets/_GAME_/Scripts/ItemSlot.cs
+++ Assets/_GAME_/Scripts/ItemSlot.cs
@@ -1,69 +1,142 @@
-using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemSlot : MonoBehaviour, IPointerClickHandler
+public class ItemSlot : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    // ====== ITEM DATA ======
     public string itemName;
     public int quantity;
     public Sprite itemSprite;
     public bool isFull;
-    
-    // ====== ITEM SLOT ======
-    [SerializeField]
-    private TMP_Text quantityText;
-    
-    [SerializeField]
-    private Image itemImage;
 
+    [SerializeField] private TMP_Text quantityText;
+    [SerializeField] private Image itemImage;
     public GameObject selectedShader;
     public bool thisItemSelected;
-    
+
     private InventoryManager inventoryManager;
-    
+    private Canvas canvas;
+    private Transform originalParent;
+    private Image dragImage;
+
     private void Start()
     {
         inventoryManager = InventoryManager.Instance;
+        canvas = GetComponentInParent<Canvas>();
     }
 
-    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    public void AddItem(string name, int qty, Sprite sprite)
     {
-        this.itemName = itemName;
-        this.quantity = quantity;
-        this.itemSprite = itemSprite;
+        itemName = name;
+        quantity = qty;
+        itemSprite = sprite;
         isFull = true;
 
-        quantityText.text = quantity.ToString();
+        itemImage.enabled = true;
+        itemImage.sprite = sprite;
+
+        quantityText.text = qty.ToString();
         quantityText.enabled = true;
-        itemImage.sprite = itemSprite; 
+    }
+
+    public void ClearSlot()
+    {
+        itemName = "";
+        quantity = 0;
+        itemSprite = null;
+        isFull = false;
+
+        itemImage.sprite = null;
+        itemImage.enabled = false;
+
+        quantityText.text = "";
+        quantityText.enabled = false;
+    }
+
+    public void RefreshUI()
+    {
+        if (isFull && itemSprite != null)
+        {
+            itemImage.enabled = true;
+            itemImage.sprite = itemSprite;
+
+            quantityText.enabled = true;
+            quantityText.text = quantity.ToString();
+        }
+        else
+        {
+            ClearSlot();
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            OnLeftClick();
+            inventoryManager.DeselectAllSlots();
+            selectedShader.SetActive(true);
+            thisItemSelected = true;
         }
 
         if (eventData.button == PointerEventData.InputButton.Right)
         {
-            OnRightClick();
+            selectedShader.SetActive(false);
+            thisItemSelected = false;
         }
     }
 
-    public void OnLeftClick()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        inventoryManager.DeselectAllSlots();
-        selectedShader.SetActive(true);
-        thisItemSelected = true;
+        if (!isFull) return;
+
+        dragImage = new GameObject("DragImage").AddComponent<Image>();
+        dragImage.sprite = itemSprite;
+        dragImage.raycastTarget = false;
+        dragImage.transform.SetParent(canvas.transform, false);
+        dragImage.rectTransform.sizeDelta = new Vector2(64, 64);
+        dragImage.color = new Color(1, 1, 1, 0.8f);
     }
 
-    public void OnRightClick()
+    public void OnDrag(PointerEventData eventData)
     {
-        selectedShader.SetActive(false);
-        thisItemSelected = false;
+        if (dragImage != null)
+            dragImage.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (dragImage != null)
+            Destroy(dragImage.gameObject);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        var originSlot = eventData.pointerDrag?.GetComponent<ItemSlot>();
+        if (originSlot == null || originSlot == this)
+        {
+            RefreshUI();
+            return;
+        }
+
+        string tempName = itemName;
+        int tempQty = quantity;
+        Sprite tempSprite = itemSprite;
+        bool tempFull = isFull;
+
+        // Swap if slot is not empty
+        if (isFull)
+        {
+            AddItem(originSlot.itemName, originSlot.quantity, originSlot.itemSprite);
+            originSlot.AddItem(tempName, tempQty, tempSprite);
+        }
+        else // If slot is empty - move
+        {
+            AddItem(originSlot.itemName, originSlot.quantity, originSlot.itemSprite);
+            originSlot.ClearSlot();
+        }
+        
+        RefreshUI();
+        originSlot.RefreshUI();
     }
 }
