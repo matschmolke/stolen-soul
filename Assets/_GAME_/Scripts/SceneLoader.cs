f7b003d112fa930e2af58eef48d309e616bd2812
emilia186 <emiliakorn59@gmail.com>
2025-11-11 12:05:00 +0100

    fixed

--- Assets/_GAME_/Scripts/SceneLoader.cs
+++ Assets/_GAME_/Scripts/SceneLoader.cs
@@ -9,13 +9,8 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadSceneAsync("TutorialDung", LoadSceneMode.Additive);
         SceneManager.LoadSceneAsync("InventoryScene", LoadSceneMode.Additive);
         SceneManager.LoadSceneAsync("PlayerScene", LoadSceneMode.Additive);
-<<<<<<< Updated upstream
-=======
         SceneManager.LoadSceneAsync("mod1", LoadSceneMode.Additive);
         SceneManager.LoadSceneAsync("GuideScene", LoadSceneMode.Additive); //comment this
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
     }
 }
