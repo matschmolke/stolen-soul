59147ab65fc1bf4cafb69c35e31a0706b0f6eb32
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-11-15 21:35:48 +0100

    Spell casting implemented (in console).

--- Assets/_GAME_/Scripts/SpellCaster.cs
+++ Assets/_GAME_/Scripts/SpellCaster.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpellCaster : MonoBehaviour
@@ -8,6 +9,12 @@ public class SpellCaster : MonoBehaviour
 
     void Start()
     {
+        if (spells == null || spells.Length == 0)
+        {
+            Debug.LogError("No spells assigned in SpellCaster!");
+            return;
+        }
+
         cooldownTimers = new float[spells.Length];
         mainCam = Camera.main;
     }
@@ -21,7 +28,9 @@ public class SpellCaster : MonoBehaviour
     void HandleCooldowns()
     {
         for (int i = 0; i < cooldownTimers.Length; i++)
+        {
             if (cooldownTimers[i] > 0) cooldownTimers[i] -= Time.deltaTime;
+        }
     }
 
     void HandleInput()
@@ -29,7 +38,6 @@ public class SpellCaster : MonoBehaviour
         for (int i = 0; i < spells.Length; i++)
         {
             if (cooldownTimers[i] > 0) continue;
-
             if (Input.GetKeyDown(spells[i].castKey))
             {
                 Cast(i);
@@ -39,16 +47,49 @@ public class SpellCaster : MonoBehaviour
 
     void Cast(int index)
     {
-        Spell spell = spells[index];
-        Vector3 mouseWorld = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorld.z = 0;
+        if (spells == null || spells.Length <= index || spells[index] == null) return;
 
-        if (spell.spellPrefab != null)
-        {
-            GameObject proj = Instantiate(spell.spellPrefab, transform.position, Quaternion.identity);
-            proj.GetComponent<Projectile>().Shoot(mouseWorld);
-        }
+        Spell spell = spells[index];
 
         cooldownTimers[index] = spell.cooldown;
+
+        if (spell.isProjectile && spell.spellPrefab != null)
+        {
+            GameObject proj = Instantiate(spell.spellPrefab, transform.position, Quaternion.identity);
+            Projectile p = proj.GetComponent<Projectile>();
+            if (p != null)
+            {
+                Vector3 mouseWorld = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                mouseWorld.z = 0;
+                p.Shoot(mouseWorld);
+            }
+            else
+            {
+                Debug.LogWarning($"Prefab {spell.spellName} не має компонента Projectile!");
+            }
+        }
+        else if (!spell.isProjectile)
+        {
+            if (spell.spellName == "Invisibility")
+            {
+                StartCoroutine(ActivateInvisibility(spell.cooldown / 2));
+            }
+            else if (spell.spellName == "Heal")
+            {
+                Heal(spell.manaCost);
+            }
+        }
+    }
+
+    IEnumerator ActivateInvisibility(float duration)
+    {
+        Debug.Log("Invisibility activated!");
+        yield return new WaitForSeconds(duration);
+        Debug.Log("Invisibility ended!");
+    }
+
+    void Heal(float amount)
+    {
+        Debug.Log($"Heal {amount} HP!");
     }
 }
