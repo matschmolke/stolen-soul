6eacadcfe4d6101b53b91b743fcd90c281af1a35
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-11-16 02:10:02 +0100

    Basic spells (Fireball and Ice Shard) animations.

--- Assets/_GAME_/Scripts/Projectile.cs
+++ Assets/_GAME_/Scripts/Projectile.cs
@@ -4,16 +4,25 @@ public class Projectile : MonoBehaviour
 {
     private Vector2 direction;
     public float speed;
-
+    private Animator animator;
+    
+    void Awake()
+    {
+      animator = GetComponent<Animator>();  
+    }
     public void Shoot(Vector3 target, float spellSpeed)
     {
         direction = (target - transform.position).normalized;
         speed = spellSpeed;
+
+        if (animator != null)
+        {
+            animator.SetTrigger(("CastTrigger"));
+        }
     }
 
     void Update()
     {
         transform.position += (Vector3)direction * speed * Time.deltaTime;
-        Debug.Log("Position of projectile: " + transform.position);
     }
 }
