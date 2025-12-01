2007820c83adaf59ca53d1bcafd5be65ccd90654
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-12-01 20:26:45 +0100

    Removed reduntant parts.

--- Assets/_GAME_/Scripts/Enemy/EnemyStates/EnemyChaseState.cs
+++ Assets/_GAME_/Scripts/Enemy/EnemyStates/EnemyChaseState.cs
@@ -33,6 +33,6 @@ public class EnemyChaseState : CharacterState
         }
 
         Vector2 dir = (enemy.GetPlayerPos() - enemy.GetMyPos()).normalized;
-        character.Move(dir);
+        enemy.Move(dir, enemy.Data.walkSpeed);
     }
 }
